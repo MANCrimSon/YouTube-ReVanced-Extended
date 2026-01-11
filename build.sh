1b07ed0d46bdfce93f657065e3311f5f660ab4c8
MANCrimSon <93052479+MANCrimSon@users.noreply.github.com>
2026-01-11 22:07:55 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -49,9 +49,9 @@ fi
 if ((COMPRESSION_LEVEL > 9)) || ((COMPRESSION_LEVEL < 0)); then abort "compression-level must be within 0-9"; fi
 
 rm -rf revanced-magisk/bin/*/tmp.*
-if [ "$(echo "$TEMP_DIR"/*-rv/changelog.md)" ]; then
-	: >"$TEMP_DIR"/*-rv/changelog.md || :
-fi
+for file in "$TEMP_DIR"/*/changelog.md; do
+    [ -f "$file" ] && : > "$file"
+done
 
 mkdir -p ${MODULE_TEMPLATE_DIR}/bin/arm64 ${MODULE_TEMPLATE_DIR}/bin/arm ${MODULE_TEMPLATE_DIR}/bin/x86 ${MODULE_TEMPLATE_DIR}/bin/x64
 gh_dl "${MODULE_TEMPLATE_DIR}/bin/arm64/cmpr" "https://github.com/j-hc/cmpr/releases/latest/download/cmpr-arm64-v8a"
@@ -78,12 +78,12 @@ for table_name in $(toml_get_table_names); do
 	cli_src=$(toml_get "$t" cli-source) || cli_src=$DEF_CLI_SRC
 	cli_ver=$(toml_get "$t" cli-version) || cli_ver=$DEF_CLI_VER
 
-	if ! RVP="$(get_rv_prebuilts "$cli_src" "$cli_ver" "$patches_src" "$patches_ver")"; then
+	if ! PREBUILTS="$(get_prebuilts "$cli_src" "$cli_ver" "$patches_src" "$patches_ver")"; then
 		abort "could not download rv prebuilts"
 	fi
-	read -r rv_cli_jar rv_patches_jar <<<"$RVP"
-	app_args[cli]=$rv_cli_jar
-	app_args[ptjar]=$rv_patches_jar
+	read -r cli_jar patches_jar <<<"$PREBUILTS"
+	app_args[cli]=$cli_jar
+	app_args[ptjar]=$patches_jar
 	if [[ -v cliriplib[${app_args[cli]}] ]]; then app_args[riplib]=${cliriplib[${app_args[cli]}]}; else
 		if [[ $(java -jar "${app_args[cli]}" patch 2>&1) == *rip-lib* ]]; then
 			cliriplib[${app_args[cli]}]=true
