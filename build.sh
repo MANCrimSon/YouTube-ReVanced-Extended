65a6528ddfac27096edbe48ff45785243486c320
MANCrimSon <93052479+MANCrimSon@users.noreply.github.com>
2026-02-13 18:30:14 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -168,7 +168,7 @@ if [ -z "$(ls -A1 "${BUILD_DIR}")" ]; then abort "All builds failed."; fi
 
 log "\nInstall [MicroG](https://github.com/MorpheApp/MicroG-RE/releases) for non-root YouTube and YT Music APKs"
 log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach root ReVanced YouTube and YT Music from Play Store"
-log "\n[revanced-magisk-module](https://github.com/MANCrimSon/revanced-magisk-module)\n"
+log "\n[revanced-magisk-module](https://github.com/MANCrimSon/YouTube-ReVanced-Extended)\n"
 log "$(cat "$TEMP_DIR"/*/changelog.md)"
 
 SKIPPED=$(cat "$TEMP_DIR"/skipped 2>/dev/null || :)
