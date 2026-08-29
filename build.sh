f68a4009b0878d6ee9baeaadbc6d13c1ebe30d97
MANCrimSon <93052479+MANCrimSon@users.noreply.github.com>
2026-08-30 02:46:56 +0300

    refactor: update default patches, cli source and brand to Morphe

--- build.sh
+++ build.sh
@@ -31,9 +31,9 @@ PARALLEL_JOBS=1 # TODO: multiple jobs were broken by recent cli versions. and i 
 REMOVE_RV_INTEGRATIONS_CHECKS=$(toml_get "$main_config_t" remove-rv-integrations-checks) || REMOVE_RV_INTEGRATIONS_CHECKS="true"
 DEF_PATCHES_VER=$(toml_get "$main_config_t" patches-version) || DEF_PATCHES_VER="latest"
 DEF_CLI_VER=$(toml_get "$main_config_t" cli-version) || DEF_CLI_VER="latest"
-DEF_PATCHES_SRC=$(toml_get "$main_config_t" patches-source) || DEF_PATCHES_SRC="ReVanced/revanced-patches"
-DEF_CLI_SRC=$(toml_get "$main_config_t" cli-source) || DEF_CLI_SRC="ReVanced/revanced-cli"
-DEF_RV_BRAND=$(toml_get "$main_config_t" rv-brand) || DEF_RV_BRAND="ReVanced"
+DEF_PATCHES_SRC=$(toml_get "$main_config_t" patches-source) || DEF_PATCHES_SRC="MorpheApp/morphe-patches"
+DEF_CLI_SRC=$(toml_get "$main_config_t" cli-source) || DEF_CLI_SRC="MorpheApp/morphe-desktop"
+DEF_RV_BRAND=$(toml_get "$main_config_t" rv-brand) || DEF_RV_BRAND="Morphe"
 mkdir -p "$TEMP_DIR" "$BUILD_DIR" "$STOCK_CACHE_DIR"
 
 if [ "${2-}" = "--config-update" ]; then
