0912263e975606bb0a006b28a48c1540241cbded
MANCrimSon <you@example.com>
2026-09-12 21:48:09 +0300

    fix(module): prevent system app bootloop via bind mount and improve cleanup

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -4,6 +4,8 @@ MODDIR=${0%/*}
 . "$MODDIR/config"
 
 rm -f "/data/adb/rvhc/${MODDIR##*/}.apk"
-rmdir "/data/adb/rvhc"
-
 rm -f "/data/adb/post-fs-data.d/$PKG_NAME-uninstall.sh"
+
+if ! ls /data/adb/rvhc/*.apk >/dev/null 2>&1; then
+	rm -rf "/data/adb/rvhc"
+fi
